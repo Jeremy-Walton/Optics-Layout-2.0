0a91d554e28bd00df8a2220b8a78d43839a91262
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-12-02 16:11:52 -0500

    Fix links

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
-export default {
-  // config options
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
   base: '/Optics-Layout-2.0/',
-}
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'old/index.html'),
+      },
+    },
+  },
+})
